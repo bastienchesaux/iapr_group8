cc59103e6b62e806f9e66a5883f24a6e86eb8077
mgamper <mathys.gamper@epfl.ch>
2025-05-19 12:50:28 +0200

    main.py first draft

--- project/main.py
+++ project/main.py
@@ -1,3 +1,14 @@
-## YOUR CODE
-...
+import skimage as skim
+import sklearn 
+import cv2
+import numpy as np
+import os
+
+import matplotlib.pyplot as plt
+from PIL import Image
+from tqdm import tqdm
+import napari
+%load_ext autoreload
+%autoreload 2
+import _utils
  
